553f60083c5e740e33a25c71f88caea6a523a5a7
obraia <bryan22czo@gmail.com>
2020-01-20 18:04:58 -0200

    Primeiro commit

---
+++ precache-manifest.a23dd8ba2c33a427bfe98dd3165184f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6778d2c5a138b21945e685642ed759f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b3fe52e4379ac5302c",
+    "url": "/static/css/main.1605aa8e.chunk.css"
+  },
+  {
+    "revision": "d8121b2b1e959d7b9504",
+    "url": "/static/js/2.30674bfe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.30674bfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66b3fe52e4379ac5302c",
+    "url": "/static/js/main.5e7ea83b.chunk.js"
+  },
+  {
+    "revision": "9d892c65bfdc7fb83e1a",
+    "url": "/static/js/runtime-main.d1d5d272.js"
+  }
+]);
